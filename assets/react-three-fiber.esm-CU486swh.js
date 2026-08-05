3379a4739129708c88973f4957fa5d0e0b3d8ad8
kranthi0003 <kranthi0003@users.noreply.github.com>
2026-08-05 15:11:42 +0000

    deploy: ee99f1386a3a7768028f9de66a721c58a03f8d4d

--- assets/react-three-fiber.esm-CU486swh.js
+++ assets/react-three-fiber.esm-CU486swh.js
@@ -1,4 +1,4 @@
-import{r as nt,a as qy,j as Bn}from"./index-CSWB2SyY.js";/**
+import{r as nt,a as qy,j as Bn}from"./index-rEwi1VNQ.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
