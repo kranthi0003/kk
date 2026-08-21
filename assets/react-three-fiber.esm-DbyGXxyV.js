0b1fbe7458b7b9aca721a2c935946c97243e0347
kranthi0003 <kranthi0003@users.noreply.github.com>
2026-08-21 13:57:53 +0000

    deploy: 1d489f10f109ca85ae7010b82ec18bbac8c100d8

--- assets/react-three-fiber.esm-DbyGXxyV.js
+++ assets/react-three-fiber.esm-DbyGXxyV.js
@@ -1,4 +1,4 @@
-import{r as nt,a as qy,j as Bn}from"./index-Bkx0Cbxb.js";/**
+import{r as nt,a as qy,j as Bn}from"./index-CymPuW0k.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
