637c043fcebf8bdce93256feae70825abe681bf5
kranthi0003 <kranthi0003@users.noreply.github.com>
2026-08-21 13:35:56 +0000

    deploy: ef701e0ea7aa7dfa91e929e8a36744ed0b78cfce

--- assets/react-three-fiber.esm-CLtezpJl.js
+++ assets/react-three-fiber.esm-CLtezpJl.js
@@ -1,4 +1,4 @@
-import{r as nt,a as qy,j as Bn}from"./index-DBFyjUGP.js";/**
+import{r as nt,a as qy,j as Bn}from"./index-P4fKZx9N.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
